0ab7d05f792c44afd8546fef03104b4a191cf83d
chipo <pnapoli33@gmail.com>
2020-10-19 14:50:13 -0300

    video #76 - In app.module.ts I define the routes and configure the router based on the given routes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,24 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'category/:id', component: ProductListComponent },
+  { path: 'category', component: ProductListComponent },
+  { path: 'products', component: ProductListComponent },
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: '**', redirectTo: '/products', pathMatch: 'full' },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductListComponent
-  ],
+  declarations: [AppComponent, ProductListComponent],
   imports: [
-    BrowserModule,
+    RouterModule.forRoot(routes),
+    BrowserModule, 
     HttpClientModule
   ],
   providers: [ProductService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
