c63382cd4064f58f45ac5c45cd008a1f3417b619
chipo <pnapoli33@gmail.com>
2020-10-20 16:06:08 -0300

    video 96.0 - In product-list.component.ts add handleSearchProducts() method and modify listProducts(); In product.service.ts add searchProducts() method to call the backend endpoint.

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -31,6 +31,15 @@ export class ProductService {
     );
   }
 
+  searchProducts(theKeyword: string): Observable<Product[]>  {
+    // Build the URL based on the keyword.
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
+
 }
 
 interface GetResponseProducts {
