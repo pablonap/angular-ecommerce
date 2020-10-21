bf3ba4edf626b5e1abd40addddbdba6eb18cb0bb
chipo <pnapoli33@gmail.com>
2020-10-21 16:30:48 -0300

    >>> ng generate component components/cart-status

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CartStatusComponent } from './components/cart-status/cart-status.component'
 
 const routes: Routes = [
   { path: 'products/:id', component: ProductDetailsComponent },
@@ -22,7 +23,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, ProductCategoryMenuComponent, SearchComponent, ProductDetailsComponent],
+  declarations: [AppComponent, ProductListComponent, ProductCategoryMenuComponent, SearchComponent, ProductDetailsComponent, CartStatusComponent],
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule, 
